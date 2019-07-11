dd2888b9f8edc4b061d58b8432205467bed58541
Hunor <hunor@unumotors.com>
2019-07-11 23:10:17 +0200

    Initial

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = {
+  "2019-07-11": [
+    {
+      name: "KitKat",
+      event: "Gegen",
+      lineup: "Famous DJ"
+    },
+    {
+      name: "Berghain",
+      event: "Christmas party",
+      lineup: "Another Famous DJ"
+    }
+  ]
+}
