46f78c45c785c50de20799188271bfa407660d4a
Sqrz-ent <52811762+Sqrz-ent@users.noreply.github.com>
2019-07-21 19:53:09 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,515 +1,490 @@
  const data = {
-  "2019-07-15": [
-    {
+  "2019-07-22": [
       name: "Pacha",
       event: "Masquerade",
-      lineup: "Claptone, Maya, Jane Coles, Heidi, Sidney Charles, Joyce Muniz"
-    },
-    {
+      lineup: "Claptone, Todd Terry, Nora En Pure, Weiss"
+    ],
+    [
       name: "Amnesia",
       event: "Cocoon Piramide",
-      lineup: "LEN FAKI, CHARLOTTE DE WITTE, MATADOR, MAR FLORES, FOUR TET, RHADOO, TINI"
-    },
-    {
+      lineup: "DUBFIRE, ANNA, JOSH WINK, SVEN VATH, TALE OF US"
+    ],
+    [
       name: "Lio",
       event: "Liamme",
       lineup: "Angel Linde, Lunnas, Mr Doris, Willie Graff"
-    },
-    {
+    ],
+    [
       name: "Hi Ibiza",
       event: "Rich Bitch",
-      lineup: "Trey Songz, Ayo & Teo, Ale Zuber, Cristian Ferreti, Dirty Nick, DJ Charlesy, DJ D-One, DJ Policy, Don Kino, Fogon Soundsystem, Francesca Giglio, Hains, Joseph, Kay, Pierpaolo Bonelli"
-    },
-    {
+      lineup: "Future, Ale Zuber, Cre-8, Cristian Ferreti, DJ Charlesy, DJ D-One, DJ Policy, Don Jiggy, Don Kino, DJ Fabrizio, Fogon Soundsystem, Hains, Joseph, Kay, Kento, Leslie, Manu El Comandante, Talinka"
+    ],
+    [
       name: "Ushuaia",
       event: "Big David Guetta",
-      lineup: "David Guetta, The Cainsmokers, Jonas Blue, Lovra"
-    },
-    {
+      lineup: "David Guetta, Oliver Heldens, Tom Staar, Lovra"
+    ],
+    [
       name: "Eden",
       event: "Idol Ibiza",
-      lineup: "N´TO, Joachim Pastor, Worakls, King Joshua, Diego Donati, Murjd"
-    },
-    {
+      lineup: "Orbital, King Joshua, Diego Donati, Murjd"
+    ],
+    [
       name: "Ibiza Rocks",
       event: "MK Area 10",
-      lineup: "MK, TODD TERRY, WEISS, NORA EN PURE , NIGHTLAPSE"
-    },
-    {
+      lineup: "MK, HANNAH WANTS, OFFAIAH, ICARUS, SAFFRON STONE"
+    ],
+    [
       name: "Es Paradis",
-      event: "Los 4 de Cuba",
-      lineup: "DJ Sherlock, La Negra DJ, DJ Legendario, Silvina "
-    },
-    {
+      event: "Glow",
+      lineup: "MDKIbiza & Yor D"
+    ],
+    [
       name: "O Beach",
       event: "Hed Kendi",
       lineup: "Loeca, Tom Crane, Grant collins – Manual Moore on guitar, Lorena De Tena on piano, Ruben on Sax and Micah on the Violin"
-    },
-    {
-      name: "Heart",
-      event: "Electrico Romantico",
-      lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Swag",
       event: "we love black music",
       lineup: "TBA"
-    }
-  ]
-}
+    ],  
+      [
+      name: "Heart Ibiza",
+      event: "Electico romantico",
+      lineup: "Bob Sinclair, Kolombo, Mark Ursa ,Fredelux"
+    ]
 
 
-
-"2019-07-16": [
-    {
+"2019-07-23": [
+      name: "Privilege",
+      event: "Resistence",
+      lineup: "Adam Beyer B2B Maceo Plex, Sasha | John Digweed, Patrice Bäumel, Charlotte De Witte, Eats Everything, Hot Since 82, Layo & Bushwacka!, Marco Bailey"
+    ],
+     [
       name: "Pacha",
       event: "Latino Gang",
-      lineup: "Lunay, Khea, Chris Jeday, Gianluca, Vacchi"
-    },
-    {
+      lineup: "J Balvin, Sky"
+    [,
+    ]
       name: "Heart Ibiza",
       event: "Heart Factory",
-      lineup: "TBA"
-    },
-    {
+      lineup: "Behrouz, Hernan Cattaneo, Nick Warren, Viton"
+    [,
+    ]
       name: "Amnesia",
       event: "Together",
-      lineup: "ANDY C, PENDULUM, HOLY GOOF, MY NU LENG WITH DREAD MC, PLACES & FACES, UNKNOWN T , AFE SMALLZ, FLOHIO, AMY BECKER, PREDITAH"
-    },
-    {
+      lineup: "CHASE & STATUS, ANDY C, TDQ, MY NU LENG WITH DREAD MC, JAMES HYPE, NOTION, JAUZ B2B GOOF"
+    [,
+    ]
       name: "Hi Ibiza",
       event: "Bodyworks",
-      lineup: "Camel Phat, Solardo, Cristoph, Del-30, Fisher, Martin Ikin, Cloonee, Francisco Allendes"
-    },
-    {
+      lineup: "Camel Phat, Solardo, Chelina Manuhutu, Chriss Mc Gee, Fisher, Monki, Pirate Copy, Manu Gonzales"
+    ],
+    [
       name: "Swag",
       event: "Mash Me Up",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Ushuaia",
       event: "Dimitri & Like Mike",
-      lineup: "Dimitri Vegas & Like Mike, Afrojack, Loud Luxury, Mahmut Orhan, BonHaus"
-    },
-    {
-      name: "Cova Santa",
-      event: "Guardians of EK ",
-      lineup: "TBA"
-    },
-    {
+      lineup: "Dimitri Vegas & Like Mike, Lost Frequencies, DJ Diesel, Matt Medved, Lost Stories X Zaeden"
+    ],
+    [
       name: "Ibiza Rocks",
       event: "Craig David Pool Party",
-      lineup: "Craig David , YUNGEN MAJESTIC, PATRICK NAZEMI, K1R3Y"
-    },
-    {
+      lineup: "CRAIG DAVID, B YOUNG, DJ TARGET, PATRICK NAZEMI, K1R3Y"
+    ],
+    [
       name: "O Beach Ibiza",
       event: "Kisstory",
       lineup: "Billy Da Kid"
-    },
-    {
+    ],
+    [
       name: "Es Paradis",
-      event: "Bringing de Party back",
+      event: "Only Groove",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Essigi",
       event: "Progressive",
       lineup: "TBA"
-    },
-    {
-      name: "Eden",
-      event: "Midsummerparty",
-      lineup: "Art Department, Infinity Ink, Leon, Gianni Callipari and Bec"
-    },
-    {
+    ],
+    [
       name: "Lio",
       event: "Pop Art",
       lineup: "Betto Music, Marina Simioni"
-    },
-    {
+    ],
+    [
       name: "Cova Santa",
-      event: "Guardians of EK",
+      event: "Who Cares",
       lineup: "TBA"
-    }
-  ]
-}
+    ]
 
 
-"2019-07-17": [
-    {
+
+"2019-07-24": [
       name: "Pacha",
       event: "Flower Power",
-      lineup: "Flower Power DJs"
-    },
-    {
+      lineup: "Joan Ribas, Mariano Somoza"
+    ],
+    [
       name: "Heart Ibiza",
       event: "La Troya",
-      lineup: "TBA"
-    },
-    {
+      lineup: "Camilo Franco, Oscar Colarado"
+    ],
+    [
       name: "Amnesia",
       event: "Neon Future",
-      lineup: "STEVE AOKI, TIMMY TRUMPET, ALOK, DEORRO, HEKTOR MASS"
-    },
-    {
+      lineup: "STEVE AOKI, W&W, KAYZO, DEORRO, HEKTOR MASS"
+    ],
+    [
       name: "Lio",
       event: "Playground",
       lineup: "Toy Room"
-    },
-    {
+    ],
+    [
       name: "Hi Ibiza",
       event: "Armin Van Buren",
-      lineup: "Armin Van Buuren, Cosmic Gate, Rodg, Sunnery James & Ryan, Marciano, Bruno Martini, White Beats"
-    },
-    {
+      lineup: "Armin Van buuren, MaRLo, Estiva, Sunnery James & Roy, Marciano, Fedde Le Grand, White Beats"
+    ],
+    [
       name: "Swag",
       event: "Badgirls Club",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Ushuaia",
       event: "Elrow",
-      lineup: "Paco Osuna, Eats Everything, Marc Maya, Tini Gessler"
-    },
-    {
+      lineup: "Nic Fanciulli, Paco Osuna, Nastia, Krystal Klear, II FACES"
+    ],
+    [
       name: "Marco Polo",
       event: "We are Tune Lemonade",
       lineup: "DJ Jackson"
-    },
-    {
+    ],
+    [
       name: "Eden",
       event: "Tronic Ibiza",
-      lineup: "Cristian Smith, Ben Sims, 2 Pole, Boris, OK1"
-    },
-    {
+      lineup: "Christian Smith, Deborah De Luca, Teenage Mutans, Drunke Kong, Deepesh Sharma"
+    ],
+    [
       name: "O Beach Ibiza",
       event: "Hotbed",
-      lineup: "Parris and Lloyd,  Alex Ellenger, Remi Rose, Matt Sax, JWalks and Josh Arise"
-    },
-    {
+      lineup: "Billy Cocks, Danny Langan, Lloyd, Alex Ellenger, Matt Sax and Bongo Ben"
+    ],
+    [
       name: "Es Paradis",
       event: "Fiesta del agua",
       lineup: "Steeve Valverde"
-    },
-    {
+    ],
+    [
       name: "Essigi",
       event: "TechHouse",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Ibiza Rocks",
       event: "Bongo Bingo",
       lineup: "Bongo Bingo Residents"
-    },
-    {
+    ],
+    [
       name: "Privilege",
       event: "Manycome",
-      lineup: "Amber, Juliana Yamasaki, Tania Moon, Taia"
-    }
-  ]
-}
+      lineup: "Lilly Palmer, Nusha"
+    ]
 
 
-"2019-07-18": [
-    {
+
+"2019-07-25": [
       name: "Pacha",
       event: "Music On",
-      lineup: "Marco Carola, Joseph Capriati, Vanjee"
-    },
-    {
+      lineup: "Marco Carola, Danny Tenaglia, Bill Patrick"
+    ],
+    [
       name: "Heart Ibiza",
       event: "Maktup Heart",
-      lineup: "TBA"
-    },
-    {
+      lineup: "Satori"
+    ],
+    [
       name: "Amnesia",
       event: "Abode",
-      lineup: "GORGON CITY, AMINE EDGE & DANCE, MAX CHAPMAN, GW HARRISON"
-    },
-    {
+      lineup: "RICHY AHMED, SANTE, MAR-T, MASON COLLECTIVE"
+    ],
+    [
       name: "Lio",
       event: "Night Fever",
       lineup: "Felix Da HouseCat"
-    },
-    {
+    ],
+    [
       name: "Hi Ibiza",
       event: "Afterlife",
-      lineup: "Tale of US, Âme, Mind against, Mathame, Nina Kravitz, Sterac, Vril, Alex.Do"
-    },
-    {
+      lineup: "Tale of US, Dixon, Denis Horvat, Colyn, Ben Klock, Rodhâd, Ryan Elliot, Vatican Shadow"
+    ],
+    [
       name: "Swag",
       event: "Xcandalous",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Destino",
       event: "Destino Music On",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Ushuaia",
       event: "Martin Garrix",
-      lineup: "Martin Garrix, Louis The Child, Seth Hills, Infuze, Brody Jenner"
-    },
-    {
+      lineup: "Martin Garrix, Vintage Culture, DubVision, Osrin, Todd Helder"
+    ],
+    [
       name: "Ibiza Rocks",
       event: "Rudimental",
-      lineup: "RUDIMENTAL, JAMES ARTHUR, DJ TARGET, PATRICK NAZEMI, K1R3Y"
-    },
-    {
+      lineup: "RUDIMENTAL, STEFFLON DON, CHARLIE SLOTH, PATRICK NAZEMI, K1R3Y"
+    ],
+    [
       name: "O Beach Ibiza",
       event: "One",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Es Paradis",
       event: "Boom Boom Live",
       lineup: "Sugarhill Gang, Grandmaster Mele Mel & Scorpio of Furious 5, Baby D"
-    },
-    {
+    ],
+    [
       name: "Essigi",
       event: "Techno",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Eden",
       event: "Shine",
       lineup: "Paul Van Dyk"
-    },
-  ]
-}
+    ],
+   [
+      name: "Cova Santa",
+      event: "Dailycid",
+      lineup: "DJ Wild"
+    ]
 
 
-"2019-07-19": [
-    {
+
+"2019-07-26": [
       name: "Pacha",
       event: "Transmoderna",
-      lineup: "Dixon, Kink Live, Terr"
-    },
-    {
+      lineup: "Dixon, Virgil, Abloh, Na´Sayah"
+    ],
+    [
       name: "Heart Ibiza",
       event: "B!Crazy",
-      lineup: "TBA"
-    },
-    {
+      lineup: "Jean Claude Ades, Moscoman, Amine K"
+    ],
+    [
       name: "Amnesia",
       event: "Do not sleep Vagabundos",
-      lineup: "SOUL CLAP, CARUAN, LUCIANO, CLAUDE VONSTROKE"
-    },
-    {
+      lineup: "OMAR S, DJ HOLOGRAPHIC, IDRISS D, LUCIANO, TINI"
+    ],
+    [
       name: "Lio",
       event: "Vintage",
       lineup: "Sebastian Gamboa"
-    },
-    {
+    ],
+    [
       name: "Hi Ibiza",
       event: "F**k me I´m famous",
-      lineup: "David Guetta, Bob Sinclair, Cedric Gervais, JP Candela"
-    },
-    {
+      lineup: "David Guetta, Robin Schulz, Morten, JP Candela"
+    ],
+    [
       name: "Swag",
       event: "Unplanned ",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Hard Rock Hotel",
       event: "Children of the 80s",
       lineup: "Jose Maria Castells, Toni Peret, Quique Tejada, Petit & Vazquez"
-    },
-    {
+    ],
+    [
       name: "Cova Santa",
       event: "Shadows",
-      lineup: "Magdalena, Tiefschwarz, Audiofly, Nolah, Stereocalypse"
-    },
-    {
+      lineup: "Magdalena, Solomun"
+    ],
+    [
       name: "Eden",
       event: "Defected in the House",
-      lineup: "Todd Edwards, Sam Divine, Low Steppa, David Penn"
-    },
-    {
+      lineup: "Gorgon City, Shiba San, Robosonic, Armand Van Helden"
+    ],
+    [
       name: "Ibiza Rocks",
       event: "Cuckoo Land",
-      lineup: "sonny Fodera, DOM DOLLA, ILLYUS & BARIENTOS, DREW MORELAND, PAT HICKEY, DAN SEAMAN"
-    },
-    {
+      lineup: "SONNY FODERA, DOM DOLLA, FRIEND WITHIN, GRIFF, CRAIG HARRISON"
+    ],
+    [
       name: "Es Paradis",
       event: "Urban Policy",
       lineup: "DJ Policy, DJ Azza, DJ Tango, Spina B, DJ Bubba, Hains, Grant Collins"
-    },
-    {
+    ],
+    [
       name: "Essigi",
       event: "Drum N Bass",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Privilege",
       event: "Supermartxe",
-      lineup: "David Morales, The Cube Guys, Abel The Kid, Javi Reina, Nalaya"
-    },
-    {
+      lineup: "Cedric Gervais, Though Love, Juanjo Martin, Abel The Kid, Nalaya"
+    ],
+    [
       name: "Ushuaia",
-      event: "Sven Väth",
-      lineup: "Sven Väth, Boris Brejcha, Ann Clue, Cesar Vincent"
-    },
-    {
+      event: "Only Once",
+      lineup: "Sweish House Mafia, Axwell, Steve Angello and Sebastian Ingrosso“
+    ],
+    [
       name: "Benimussa Park",
-      event: "ClockWork Orange",
-      lineup: "TBA"
-    }
+      event: "Love Amplified",
+      lineup: "Heidi, Dusky, Melé, Josh Butler, Soul Divide, D´Julz, DJ Steaw, Black Loops, Dennis Quin"
   ]
-}
 
 
-"2019-07-20": [
-    {
+
+"2019-07-27": [
       name: "Privilege",
       event: "Brutal",
-      lineup: "Wisin & Yandel, Zion & Lennox, Karol G, Farruko"
-    },
-    {
+      lineup: "Manuel Turizo"
+    ],
+    [
       name: "Pacha",
       event: "Pure Pacha",
-      lineup: "Roger Sanchez, DJ Sneak, Junior Sanchez, Mambo Brothers, Kristen Knight, Sebastian Gamboa"
-    },
-    {
+      lineup: "Martin Solveig, 2ManyDJs, Totally Enormous Extinct Dinosaurs, Sebastian Gamboa"
+    ],
+    [
       name: "Heart Ibiza",
       event: "Wildchild",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Amnesia",
       event: "Elrow",
-      lineup: "KÖLSCH, VICTOR CALDERONE, TECHNASIA, WAFF, TINI GESSLER"
-    },
-    {
+      lineup: "MATTHIAS TANZMANN, LATMUN, RIVA STARR, WADE, ILARIO ALICANTE"
+    ],
+    [
       name: "Lio",
       event: "Liamme",
       lineup: "Angel Linde, Lunnas, Mr Doris, Willie Graff"
-    },
-    {
+    ],
+    [
       name: "Hi Ibiza",
       event: "Black Coffee",
-      lineup: "Black Coffee, Damian Lazarus, Enzo Siragusa, Lazare Hoche, SecondCity"
-    },
-    {
+      lineup: "Black Coffee, Black Madonna, Culoe de Song, Dorian Craft, Themba, Vanjee"
+    ],
+    [
       name: "Swag",
       event: "Soul To Soul",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Ushuaia",
       event: "Ants",
-      lineup: "Andrea Oliva, Butch, Davide Squillace, Dublin, Emanuel Satie, Erick Morillo"
-    },
-    {
+      lineup: "Andrea Oliva, Davide Squillace, Lauren Lane, Maya Jane Coles Raúl Rodríguez, Tiga"
+    ],
+    [
       name: "Eden",
       event: "WNDRLND Eden",
-      lineup: "Andy Joyce, James iD, DAYS, Cansen, FAB"
-    },
-    {
+      lineup: "Paul Taylor, Jon Dasilva, Ricky Isted, Juliet Roberts"
+    ],
+    [
       name: "O Beach Ibiza",
       event: "Soul Heaven",
-      lineup: "TBA"
-    },
-    {
+      lineup: "Tom Crane, Grant Collins, Super Lover"
+    ],
+    [
       name: "Es Paradis",
       event: "White Island",
       lineup: "The White Island Orchestra"
-    },
-    {
+    ],
+    [
       name: "Essigi",
       event: "Deep & Tribal",
       lineup: "TBA"
-    },
-    {
-      name: "Cova Santa",
-      event: "El Fabuloso",
-      lineup: "Carlos Areces, Nancy Rubias, Lorena Castell, Silvia Superstar, The Parrots DJ Set"
-    },
-    {
+    ],
+    [
       name: "Ibiza Rocks",
       event: "Applebum bottomless Brunch",
-      lineup: "DJ Policy, Danny Beck"
-    },
-    {
-      name: "Benimussa Park",
-      event: "Senssual Ibiza",
-      lineup: "TBA"
-    }
+      lineup: "SPINNA B, DANNY BECK"
   ]
-}
 
 
-"2019-07-21": [
-    {
+
+
+"2019-07-28": [
       name: "Pacha",
       event: "Solomun",
       lineup: "Solomun, DJ Tennis"
-    },
-    {
+    ],
+    [
       name: "Heart Ibiza",
       event: "Saga",
-      lineup: "Bedouin, Satori, Braunbeck"
-    },
-    {
+      lineup: "Bedouin, Matthias Meyer"
+    ],
+    [
       name: "Amnesia",
       event: "Clockwork",
-      lineup: "PAUL OAKENFOLD, JEREMY HEALY, GRAEME PARK, SEB FONTAINE, TALL PAUL, SMOKIN JO, ALFREDO, BRANDON BLOCK Y ALEX P, JASON BYE, DANNY CLOCKWORK, ANDY MANSTON"
-    },
-    {
+      lineup: "LIL' PAUL DJ, JASON MATA, SMITH & SMILE , M3 SHOW: MICAH THE VIOLINIST, RUBEN MORAN SAX, MANUEL MOORE"
+    ],
+    [
       name: "Hi Ibiza",
       event: "Glitterbox",
-      lineup: "Basement Jaxx, Honey Dijon, Dimitri From Paris, Joey Negro, Crazy P, Melon Bomb, Fiorious"
-    },
-    {
+      lineup: "Basement Jaxx, dimitri From Paris, Jellybean Benitez, The Shapeshifters, Eli Escobar, Sophie Lloyd, Nadia Ksaiba"
+    ],
+    [
       name: "Swag",
       event: "Safari",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Ushuaia",
       event: "Mucho",
       lineup: "Ozuna, Ale Zuber, Kay, Fogon Soundsystem, DJ D-One"
-    },
-    {
+    ],
+    [
       name: "Cova Santa",
       event: "Woomoon",
-      lineup: "Sahalé, Huaira, BLOND:ISH, Niki Sadeki, Armen Miran, Oliver Koletzki"
-    },
-    {
+      lineup: "Iorie,  Monolink,  Sainte Vie,  Nour,  Anstascia,  Holmar "
+    ],
+    [
       name: "Eden",
       event: "ToolRoom Ibiza",
-      lineup: "Danny Howard, Leftwing:Kody, Mark Knight, Maxinne, PAX"
-    },
-    {
+      lineup: "Adrian Hour, Dirty South, Mark Knight, Maxinne, Wheats"
+    ],
+    [
       name: "O Beach ",
       event: "House Paradise",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Benimussa Park",
       event: "The Zoo Project",
       lineup: "Traumer, Giuliano Lomonte, Brawther, Defex, Real Gang Soundsystem"
-    },
-    {
+    ],
+    [
       name: "Es Paradis",
       event: "Fiesta Del Agua",
       lineup: "TBA"
-    },
-    {
+    ],
+    [
       name: "Lio",
       event: "MaxiDiscoteca",
       lineup: "2ManyDJs"
-    },
-    {
+    ],
+    [
       name: "Privilege",
       event: "Solid Grooves",
-      lineup: "DJ Deeon, Mason Maynard, Michael Bibi, PAWSA, Route 94"
-    },
-    {
+      lineup: "Anotr, Josh Butler, Luuk Van Dijk, Michael Bibi, Rebuke"
+    ],
+    [
       name: "Essigi",
       event: "Old School",
       lineup: "TBA"
-    },
   ]
 }
