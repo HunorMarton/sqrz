bcabbc3f4d8bfd8f6a6fca16886d29ee4494675a
Sqrz-ent <52811762+Sqrz-ent@users.noreply.github.com>
2019-07-21 20:15:00 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,11 @@
 const data = {
-  "2019-07-21": {[
+  "2019-07-21": {
       name: "wejiof",
       event: "cdsajiöcoafji",
       lineup: "Claptone, Todd Terry, Nora En Pure, Weiss"
-    ],
-    [
+    },
+    {
       name: "ndeiofpejwifo",
       event: "ndjqwdnjlwmide",
       lineup: "DUBFIRE, ANNA, JOSH WINK, SVEN VATH, TALE OF US"
-    ],
 }
