c98cf3cdb66cb0c201d5ab66bd393a52a4ce2392
Sqrz-ent <52811762+Sqrz-ent@users.noreply.github.com>
2019-07-21 20:16:40 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,57 @@
 const data = {
-  "2019-07-21": {
-      name: "wejiof",
-      event: "cdsajiöcoafji",
+  "2019-07-22": [
+      name: "Pacha",
+      event: "Masquerade",
       lineup: "Claptone, Todd Terry, Nora En Pure, Weiss"
-    },
-    {
-      name: "ndeiofpejwifo",
-      event: "ndjqwdnjlwmide",
+    ],
+    [
+      name: "Amnesia",
+      event: "Cocoon Piramide",
       lineup: "DUBFIRE, ANNA, JOSH WINK, SVEN VATH, TALE OF US"
+    ],
+    [
+      name: "Lio",
+      event: "Liamme",
+      lineup: "Angel Linde, Lunnas, Mr Doris, Willie Graff"
+    ],
+    [
+      name: "Hi Ibiza",
+      event: "Rich Bitch",
+      lineup: "Future, Ale Zuber, Cre-8, Cristian Ferreti, DJ Charlesy, DJ D-One, DJ Policy, Don Jiggy, Don Kino, DJ Fabrizio, Fogon Soundsystem, Hains, Joseph, Kay, Kento, Leslie, Manu El Comandante, Talinka"
+    ],
+    [
+      name: "Ushuaia",
+      event: "Big David Guetta",
+      lineup: "David Guetta, Oliver Heldens, Tom Staar, Lovra"
+    ],
+    [
+      name: "Eden",
+      event: "Idol Ibiza",
+      lineup: "Orbital, King Joshua, Diego Donati, Murjd"
+    ],
+    [
+      name: "Ibiza Rocks",
+      event: "MK Area 10",
+      lineup: "MK, HANNAH WANTS, OFFAIAH, ICARUS, SAFFRON STONE"
+    ],
+    [
+      name: "Es Paradis",
+      event: "Glow",
+      lineup: "MDKIbiza & Yor D"
+    ],
+    [
+      name: "O Beach",
+      event: "Hed Kendi",
+      lineup: "Loeca, Tom Crane, Grant collins – Manual Moore on guitar, Lorena De Tena on piano, Ruben on Sax and Micah on the Violin"
+    ],
+    [
+      name: "Swag",
+      event: "we love black music",
+      lineup: "TBA"
+    ],  
+      [
+      name: "Heart Ibiza",
+      event: "Electico romantico",
+      lineup: "Bob Sinclair, Kolombo, Mark Ursa ,Fredelux"
+    ]
 }
