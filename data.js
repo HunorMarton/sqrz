9868f416ae168c70a7784a3cb10306ad2fc66e30
Sqrz-ent <52811762+Sqrz-ent@users.noreply.github.com>
2019-07-24 17:24:23 -0400

    Update data.js

--- data.js
+++ data.js
@@ -22,7 +22,32 @@ event: "Christmas party",
 
 lineup: "Another Famous DJ"
 
+}
+
+],
+  
+  
+  "2019-07-25": [
+
+{
+
+name: "KitKat",
+
+event: "Gegen",
+
+lineup: "Famous DJ"
+
 },
+
+{
+
+name: "Berghain",
+
+event: "Christmas party",
+
+lineup: "Another Famous DJ"
+
+}
 
 ]
 
