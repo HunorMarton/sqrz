6af8c3cc6703c4ea3557f7088f4ed4289927488b
Sqrz-ent <52811762+Sqrz-ent@users.noreply.github.com>
2019-07-24 17:24:49 -0400

    Update data.js

--- data.js
+++ data.js
@@ -2,32 +2,7 @@
 const data = {
 
 
-"2019-07-24": [
-
-{
-
-name: "KitKat",
-
-event: "Gegen",
-
-lineup: "Famous DJ"
-
-},
-
-{
-
-name: "Berghain",
-
-event: "Christmas party",
-
-lineup: "Another Famous DJ"
-
-}
-
-],
-  
-  
-  "2019-07-25": [
+"2019-07-11": [
 
 {
 
