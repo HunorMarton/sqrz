9c42a9a55d288894dbe340881217b2e2493c0433
Hunor Márton Borbély <bor.hunor@gmail.com>
2019-07-27 21:31:34 +0200

    Test caching

--- data.js
+++ data.js
@@ -1,29 +1,27 @@
 
 const data = {
-
-
-"2019-07-27": [
-
-{
-
-name: "KitKat",
-
-event: "Gegen",
-
-lineup: "Famous DJ"
-
-},
-
-{
-
-name: "Berghain",
-
-event: "Christmas party",
-
-lineup: "Another Famous DJ"
-
-}
-
-]
-
+  "2019-07-27": [
+    {
+      name: "KitKat",
+      event: "Gegen",
+      lineup: "Famous DJ"
+    },
+    {
+      name: "Berghain",
+      event: "Christmas party",
+      lineup: "Another Famous DJ"
+    }
+  ],
+  "2019-07-28": [
+    {
+      name: "KitKat",
+      event: "Gegen",
+      lineup: "Famous DJ"
+    },
+    {
+      name: "Berghain",
+      event: "Christmas party",
+      lineup: "Another Famous DJ"
+    }
+  ]
 }
